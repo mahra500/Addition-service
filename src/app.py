0913fb1fc8abca6eda862333b093bd27317b00cd
mahra500 <mahraaman6@gmail.com>
2021-12-22 23:58:59 +0100

    changes for correct endpoint

--- src/app.py
+++ src/app.py
@@ -8,4 +8,4 @@ def create_app():
     return app
 
 if __name__ == "__main__":
-    create_app().run(host='localhost',  port=5000, debug=True)
+    create_app().run(host='0.0.0.0',  port=5000, debug=True)
