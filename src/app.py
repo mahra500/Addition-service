708f3c27ca4ee6a9f68330fe25507f315a016b06
mahra500 <mahraaman6@gmail.com>
2021-12-22 23:09:05 +0100

    First commit

---
+++ src/app.py
@@ -0,0 +1,11 @@
+from flask import Flask, request
+from blueprints.addition import additon_bp
+
+def create_app():
+    app = Flask(__name__)
+    app.register_blueprint(additon_bp.addition_bp)
+
+    return app
+
+if __name__ == "__main__":
+    create_app().run(host='localhost',  port=5000, debug=True)
